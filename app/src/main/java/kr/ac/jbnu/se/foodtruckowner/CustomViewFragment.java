bf1bb56f459c99370c38c320be36c65e53a58696
HyeonPyo <khp915@nate.com>
2016-10-12 15:34:56 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/jbnu/se/foodtruckowner/CustomViewFragment.java
@@ -0,0 +1,20 @@
+package kr.ac.jbnu.se.foodtruckowner;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CustomViewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.CustomView_layout, null);
+    }
+}
