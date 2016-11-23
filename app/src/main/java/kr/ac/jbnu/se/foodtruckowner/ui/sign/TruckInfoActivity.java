b37d17984808146ff405daf5eb3c9164ea07a41e
HwangKwang <sjjongg123@gmail.com>
2016-11-24 03:42:44 +0900

    푸드트럭입력창 만들다가만ㅎ

--- app/src/main/java/kr/ac/jbnu/se/foodtruckowner/ui/sign/TruckInfoActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/foodtruckowner/ui/sign/TruckInfoActivity.java
@@ -1,9 +1,13 @@
 package kr.ac.jbnu.se.foodtruckowner.ui.sign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import kr.ac.jbnu.se.foodtruckowner.R;
+import kr.ac.jbnu.se.foodtruckowner.ui.MainActivity;
 
 public class TruckInfoActivity extends AppCompatActivity {
 
@@ -11,6 +15,21 @@ public class TruckInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_truck_info);
+
+        Intent truckintent = getIntent();
+
+        Button truckbtn = (Button) findViewById(R.id.truckbtn);
+
+        truckbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(TruckInfoActivity.this, SigninActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+
+
 
     }
 }
