77ef12d663f8a824549e4117af212fe3f3c9f7d4
DH <dohun94@naver.com>
2016-11-25 03:58:38 +0900

    도현이 등장! 회원가입 db연동 완료!

--- app/src/main/java/kr/ac/jbnu/se/foodtruckowner/ui/sign/SigninActivity.java
+++ app/src/main/java/kr/ac/jbnu/se/foodtruckowner/ui/sign/SigninActivity.java
@@ -8,14 +8,24 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import kr.ac.jbnu.se.foodtruckowner.R;
+import kr.ac.jbnu.se.foodtruckowner.model.Owner;
+import kr.ac.jbnu.se.foodtruckowner.service.ApiService;
 import kr.ac.jbnu.se.foodtruckowner.ui.MainActivity;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 import static java.sql.Types.NULL;
 
@@ -24,11 +34,13 @@ public class SigninActivity extends AppCompatActivity {
     private SharedPreferences pref_id_store;
     private SharedPreferences pref_store_id;
     private SharedPreferences.Editor editor;
+
     private Toolbar toolbar;
     boolean at_login = false;
     private Switch sw_id_store;
     private Switch sw_auto_login;
-    private EditText et_id;
+    private EditText et_email;
+    private EditText et_password;
 
 
     @Override
@@ -36,26 +48,32 @@ public class SigninActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
         overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
+
         mPref = PreferenceManager.getDefaultSharedPreferences(this);
         at_login = mPref.getBoolean("auto_login", false);
         pref_id_store = getSharedPreferences("id_store", NULL);
         pref_store_id = getSharedPreferences("store_id", NULL);
-        et_id = (EditText) findViewById(R.id.et_signin_email);
+        et_email = (EditText) findViewById(R.id.et_signin_email);
+        et_password = (EditText) findViewById(R.id.et_signin_pw);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("로그인");
         toolbar.setTitleTextColor(Color.WHITE);
+
         sw_auto_login = (Switch) findViewById(R.id.sw_auto_log);
         sw_id_store = (Switch) findViewById(R.id.sw_stroe_id);
         setSupportActionBar(toolbar);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
         //actionBar.setHomeAsUpIndicator(R.drawable.button_back); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
+
         sw_auto_login.setChecked(at_login);
-        sw_id_store.setChecked(pref_id_store.getBoolean("id_store",false));
-        if (pref_id_store.getBoolean("id_store",false)) {
-            et_id.setText(pref_store_id.getString("store_id", ""));
+        sw_id_store.setChecked(pref_id_store.getBoolean("id_store", false));
+        if (pref_id_store.getBoolean("id_store", false)) {
+            et_email.setText(pref_store_id.getString("store_id", ""));
         }
         if (at_login == true) {
             auto_log();
@@ -65,21 +83,23 @@ public class SigninActivity extends AppCompatActivity {
     }
 
 
-
     public void login() {
+        //자동로그인
         if (sw_auto_login.isChecked()) {
             editor = mPref.edit();
             editor.putBoolean("auto_login", true);
             editor.commit();
         }
+        //아이디 기억
         if (sw_id_store.isChecked()) {
             editor = pref_id_store.edit();
             editor.putBoolean("id_store", true);
             editor.commit();
             editor = pref_store_id.edit();
-            editor.putString("store_id", et_id.getText().toString());
+            editor.putString("store_id", et_email.getText().toString());
             editor.commit();
-        }else {
+        } else {
+            //아이디 기억 안하니까 널값으로
             editor = pref_id_store.edit();
             editor.putBoolean("id_store", false);
             editor.commit();
@@ -87,10 +107,54 @@ public class SigninActivity extends AppCompatActivity {
             editor.putString("store_id", "");
             editor.commit();
         }
-        Toast.makeText(this, "환영합니다. 푸드트럭", Toast.LENGTH_LONG).show();
-        Intent loginIntent = new Intent(SigninActivity.this, MainActivity.class);
-        startActivity(loginIntent);
-        finish();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://server-blackdog11.c9users.io")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService service = retrofit.create(ApiService.class);
+
+        Call<Owner> convertedContent = service.request_login(et_email.getText().toString(), et_password.getText().toString());
+
+        convertedContent.enqueue(new Callback<Owner>() {
+            @Override
+            public void onResponse(Response<Owner> response, Retrofit retrofit) {
+                Log.d("Response status code: ", String.valueOf(response.code()));
+
+                // isSuccess is true if response code => 200 and <= 300
+                if (!response.isSuccess()) {
+                    // print response body if unsuccessful
+                    try {
+                        Log.d("response unsuccessful: ", response.errorBody().string());
+                    } catch (IOException e) {
+                        // do nothing
+                    }
+                    return;
+                }
+                // if parsing the JSON body failed, `response.body()` returns null
+                Owner decodedResponse = response.body();
+
+                if (decodedResponse == null) {
+                    Log.d("TAG", "아이디비번오류");
+                    Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (String.valueOf(decodedResponse.getEmail()).equals(et_email.getText().toString())) {
+                    Log.d("TAGG", "로그인성공!");
+                    Toast.makeText(getApplicationContext(), "환영합니다. 푸드트럭", Toast.LENGTH_LONG).show();
+                    Intent loginIntent = new Intent(SigninActivity.this, MainActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("실패", t.getMessage().toString());
+            }
+        });
     }
 
     public void auto_log() {
